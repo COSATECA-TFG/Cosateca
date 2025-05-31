d1522f26312f638fe82523e4cce82cba696e5420
Manuel Vélez López <manvellop2@alum.us.es>
2025-05-31 16:21:21 +0200

    feat: pantalla de home creada, bootstrap instalado correctamente

--- cosateca/core/views.py
+++ cosateca/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    """
+    Render the home page of the application.
+    """
+    return render(request, 'home.html')
